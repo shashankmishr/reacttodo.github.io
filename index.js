c48b10455b395e219c3518529b37b8be75bdacda
Shashank Mishra <shashank3oct@gmail.com>
2023-09-27 08:46:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-# reacttodo.github.io
-It will store the record of all the things you have to do in a day,
+// # reacttodo.github.io
+// It will store the record of all the things you have to do in a day,
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
